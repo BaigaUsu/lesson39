a8d4d67b133a408c90a64ffbdf14d377df1c775f
BaigaUsu <baigazy7777@gmail.com>
2024-03-01 13:06:53 +0600

    5 задача

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
  const form = document.getElementById('registration-form')
 
  form.addEventListener('submit', (event) => {
+    event.preventDefault();
     const elemsData = {
-        fullname: {},
+        fullname: {
+            pattern: /[a-z]+ [a-z]+/i
+        },
         contact: {},
         password: {},
     }
-    event.preventDefault();
-    console.log(event);
+    Object.entries(elemsData).forEach((item) => {
+        const [name, {pattern}] = item
+        const elem = form.elements[name]
+        if (pattern) {
+            const isValid = pattern.test(elem.value)
+        };
+
+    })
  })
