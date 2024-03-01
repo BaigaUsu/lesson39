7bc47036a2b4dcc8f8ed415d2b5eee8ca179fb81
BaigaUsu <baigazy7777@gmail.com>
2024-03-01 12:34:58 +0600

    4 задача

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
  const form = document.getElementById('registration-form')
 
  form.addEventListener('submit', (event) => {
+    const elemsData = {
+        fullname: {},
+        contact: {},
+        password: {},
+    }
     event.preventDefault();
     console.log(event);
  })
