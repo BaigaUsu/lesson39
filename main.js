afaf972bf726d28194b4e25940e8f81e1f76e317
BaigaUsu <baigazy7777@gmail.com>
2024-03-02 16:15:47 +0600

    6 задача

--- main.js
+++ main.js
@@ -6,7 +6,9 @@
         fullname: {
             pattern: /[a-z]+ [a-z]+/i
         },
-        contact: {},
+        contact: {
+            pattern: /^[a-z0-9.]+@[a-z0-9.-]+\.[a-z]{2,}$/i
+        },
         password: {},
     }
     Object.entries(elemsData).forEach((item) => {
@@ -14,7 +16,12 @@
         const elem = form.elements[name]
         if (pattern) {
             const isValid = pattern.test(elem.value)
+            if (!isValid) {
+                elem.classList.add('error')
+            } else {
+                elem.classList.remove('error')
+            }
         };
-
     })
  })
+
