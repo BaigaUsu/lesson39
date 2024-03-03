eba98360cbe5ef890968ada6f08b32ae9140f8a9
BaigaUsu <baigazy7777@gmail.com>
2024-03-03 15:51:33 +0600

    9 задача

--- main.js
+++ main.js
@@ -4,28 +4,37 @@
     event.preventDefault();
     const elemsData = {
         fullname: {
-            pattern: /[a-z]+ [a-z]+/i
+            pattern: /[a-z]+ [a-z]+/i,
+            message: `ФИО должно содержать как минимум два слова из русских или английских букв, с обязательным пробелом между ними`
         },
         contact: {
-            pattern: /^[a-z0-9.]+@[a-z0-9.-]+\.[a-z]{2,}$/i
+            pattern: /^[a-z0-9.]+@[a-z0-9.-]+\.[a-z]{2,}$/i,
+            message: `Пожалуйста, введите корректный адрес электронной почты.`
         },
         password: {
-            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=])[A-Za-z\d!@#$%^&*()_+\-=]{8,}$/
+            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=])[A-Za-z\d!@#$%^&*()_+\-=]{8,}$/,
+            message: `Пароль должен содержать как минимум одну заглавную букву, одну строчную букву, одну цифру и один специальный символ, и быть не менее 8 символов длиной.`
         },
     }
+    let isValid = true
+
+    const errorMessages = form.querySelectorAll('.error-message');
+    errorMessages.forEach(errorMessage => errorMessage.remove());
 
     Object.entries(elemsData).forEach((item) => {
-        const [name, {pattern}] = item
+        const [name, {pattern, message}] = item
         const elem = form.elements[name]
         if (pattern) {
-            const isValid = pattern.test(elem.value)
-            if (!isValid) {
+            const isValidElement = pattern.test(elem.value)
+            if (!isValidElement) {
                 elem.classList.add('error')
+                isValid = false;
+                displayErrorMessage(elem, message)
             } else {
                 elem.classList.remove('error')
+
             }
         };
-        return isValid = true
     })
     if (isValid) {
         form.style.display = 'none';
@@ -38,5 +47,13 @@
         messageElement.textContent = messageText;
         form.parentNode.insertBefore(messageElement, form.nextSibling);
     }
+     function displayErrorMessage(elem, message) {
+        const errorMessageElement = document.createElement('div');
+        errorMessageElement.textContent = message;
+        errorMessageElement.classList.add('error-message');
+        elem.parentNode.insertBefore(errorMessageElement, elem.nextSibling);
+    }
  })
+
+
 
